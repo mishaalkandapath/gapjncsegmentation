cc0366e259c0f9de66185221d8b5f49b2f64cafc
Mishaal Kandapath <mishaalk@cedar1.cedar.computecanada.ca>
2024-05-08 08:52:08 -0700

    removing neuron areas

--- randomstuff.py
+++ randomstuff.py
@@ -38,6 +38,28 @@ def ressample(arr, N):
         A.extend([*np.hsplit(v, arr.shape[0] // N)])
     return A
 
+def erase_neurons():
+    img_files = os.listdir(BASE+"sem_dauer_2/jnc_only_images/", sorted=True)
+    seg_files = os.listdir(BASE+"sem_dauer_2/jnc_only_seg/", sorted=True)
+    neuron_files = os.listdir(BASE+"seg_export/", sorted=True)
+    os.mkdir(BASE+"sem_dauer_2/erased_neurons/")
+    os.mkdir(BASE+"sem_dauer_2/erased_neurons/gts/")
+    os.mkdir(BASE+"sem_dauer_2/erased_neurons/imgs/")
+
+    for i in tqdm(range(len(neuron_files))):
+        img_file = neuron_files[i].replace()
+        seg_file = neuron_files[i].replace()
+        if not os.path.isfile(BASE+"sem_dauer_2/jnc_only_images/" + img_file) or not os.path.isfile(BASE+"sem_dauer_2/jnc_only_seg/" + seg_file): continue
+        img = cv2.cvtColor(cv2.imread(BASE+"sem_dauer_2/jnc_only_images/"+img_file), cv2.COLOR_BGR2GRAY)
+        neuron_mask = cv2.cvtColor(cv2.imread(BASE+"seg_export/"+neuron_files[i]), cv2.COLOR_BGR2GRAY)
+        seg = cv2.cvtColor(cv2.imread(BASE+"sem_dauer_2/jnc_only_seg/"+seg_file), cv2.COLOR_BGR2GRAY)
+        img[neuron_mask != 0] = 255
+
+        #write the new image to directory along with a seg directory 
+        cv2.imwrite(BASE+"sem_dauer_2/erased_neurons/imgs/"+img_file, img)
+        cv2.imwrite(BASE+"sem_dauer_2/erased_neurons/gts/"+seg_file, seg)
+
+
 def shorten_pics():
     counter = 0
     img_files = os.listdir(BASE+"sem_dauer_2/jnc_only_images/", sorted=True)
@@ -54,8 +76,8 @@ def shorten_pics():
         imgs, segs = ressample(img, 32), ressample(seg, 32)
         # save these 
         for i in range(len(segs)):
-            cv2.imwrite(BASE+"sem_dauer_2/tiny_jnc_only/gts/{}.png".format(counter), segs[i])
-            cv2.imwrite(BASE+"sem_dauer_2/tiny_jnc_only/imgs/{}.png".format(counter), imgs[i])
+            cv2.imwrite(BASE+"sem_dauer_2/tiny_jnc_only/gts/{}.png".format(seg_files[i][:-4] + str(counter)), segs[i])
+            cv2.imwrite(BASE+"sem_dauer_2/tiny_jnc_only/imgs/{}.png".format(img_files[i][:-4] + str(counter)), imgs[i])
             counter+=1
     
 if __name__ == "__main__":
