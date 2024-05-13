05d8f700ad95f43dd76013184bd742697a6644b8
Huayin Luo <hclover2003@gmail.com>
2024-05-13 12:25:02 -0400

    update batch scripts

---
+++ batch_scripts/unet_512_3.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --job-name=unet_512_3
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.out
+#SBATCH --gpus-per-node=v100l:1
+#SBATCH --mem=64G
+#SBATCH --time=10:0:0
+module purge
+source ~/py39/bin/activate
+module load scipy-stack gcc cuda opencv
+EPOCHS=20
+BATCH_SIZE=8
+NUM_WORKERS=4
+LR=0.001
+DATA_DIR=/home/hluo/scratch/small_data_3d_3
+MODEL_NAME="model_512_3"
+python /home/hluo/projects/def-mzhen/hluo/gapjncsegmentation/unet_3d.py --epochs $EPOCHS --batch_size $BATCH_SIZE --lr $LR --data_dir $DATA_DIR --model_name $MODEL_NAME --num_workers $NUM_WORKERS
