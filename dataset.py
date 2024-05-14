575e093b998fe4eee5a402e4100e6e046e659dfd
Huayin Luo <hclover2003@gmail.com>
2024-05-14 11:36:10 -0400

    use torchio for augment + preprocess

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np;
 import torch 
 import torchvision.transforms as transforms
+import torchio as tio
 
 class SliceDataset(torch.utils.data.Dataset):
     """ Dataset for 2D slices of 3D EM images
@@ -19,39 +20,47 @@ class SliceDataset(torch.utils.data.Dataset):
             images_dir, 
             masks_dir,
             image_dim = (5, 512, 512),
-            augmentation=None,
+            augment=False
     ):
         
         self.image_paths = [os.path.join(images_dir, image_id) for image_id in sorted(os.listdir(images_dir))]
         self.mask_paths = [os.path.join(masks_dir, image_id) for image_id in sorted(os.listdir(masks_dir))]
         self.image_dim = image_dim
-        self.augmentation = augmentation
+        self.augmentation = augment
     
     def __getitem__(self, i):
         # read images and masks (3D grayscale images)
         image = np.load(self.image_paths[i]) # each pixel is 0-255, shape (depth, height, width)
         mask = np.load(self.mask_paths[i]) # each pixel is 0 or 1, shape (depth, height, width)
-        
-        # normalize image to have mean 0 and std 1
-        try:
-            image = (image - image.mean()) / image.std() 
-        except:
-            print(f"Error in image: {self.image_paths[i]}")
-            print(f"Image shape: {image.shape}")
-            
-        
+
         # convert to tensor
         image = torch.tensor(image).float()
         mask = torch.tensor(mask).float()
 
-                
+        # define preprocessing and augmentations
+        preprocessing = tio.Compose([
+            tio.ZNormalization() # normalize image to have zero mean and unit variance
+        ])
+        mask_augment = tio.Compose([
+            tio.RandomFlip(flip_probability=0.5)
+            ])
+        img_augment = tio.Compose([
+            mask_augment,
+            tio.RandomBlur(p=0.5),
+            tio.RandomNoise(p=0.5),
+            tio.RandomGamma(p=0.5)
+        ])
+
+        image = preprocessing(image)
+        mask = preprocessing(mask)
+    
         # apply augmentations, if any
-        if self.augmentation:
-            image = self.augmentation(image)
-            mask = self.augmentation(mask)
+        if self.augment:
+            image = img_augment(image)
+            mask = mask_augment(mask)
             
         # one-hot encode the mask (depth, height, width) --> (depth, height, width, num_classes=2)
-        one_hot_mask = torch.nn.functional.one_hot(mask.long(), num_classes=2) # 0: depth, 1: height, 2: width, 3: num_classes
+        one_hot_mask = torch.nn.functional.one_hot(mask.squeeze(0).long(), num_classes=2)
         one_hot_mask = one_hot_mask.permute(3, 0, 1, 2).float() # (num_classes, depth, height, width)
         return image, one_hot_mask
         
