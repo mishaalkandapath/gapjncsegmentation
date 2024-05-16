45e5c47e58caba2a5807cc2e539380073dfd415c
hclover2003 <hclover2003@gmail.com>
2024-05-16 15:38:08 -0400

    minor changes

--- batch_scripts/job26.sh
+++ batch_scripts/job26.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
-#SBATCH --job-name=job24
+#SBATCH --job-name=job26
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.out
 #SBATCH --gpus-per-node=v100l:1
@@ -16,10 +16,10 @@ NUM_WORKERS=4
 LR=0.001
 DATA_DIR=/home/hluo/scratch/one_512
 MODEL_DIR=/home/hluo/scratch/models
-MODEL_NAME="model_job24_overfit"
+MODEL_NAME="model_job26_overfit"
 AUGMENT=False
 LOSS_TYPE="focal"
-ALPHA=0.002
-GAMMA=3
+ALPHA=0.2
+GAMMA=2
 NUM_PREDICTIONS_TO_LOG=10
 python /home/hluo/gapjncsegmentation/unet_3d.py --epochs $EPOCHS --batch_size $BATCH_SIZE --lr $LR --data_dir $DATA_DIR --model_name $MODEL_NAME --num_workers $NUM_WORKERS --model_dir $MODEL_DIR --alpha $ALPHA --gamma $GAMMA --num_predictions_to_log $NUM_PREDICTIONS_TO_LOG --loss_type $LOSS_TYPE
