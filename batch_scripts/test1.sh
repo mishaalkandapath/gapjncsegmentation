87548436a38b34dc297d9f3454d48effb9393e2b
Huayin Luo <hclover2003@gmail.com>
2024-05-17 11:21:30 -0400

    test

--- batch_scripts/test1.sh
+++ batch_scripts/test1.sh
@@ -6,11 +6,11 @@
 #SBATCH --error=%x-%j.out
 #SBATCH --gpus-per-node=v100l:1
 #SBATCH --mem=64G
-#SBATCH --time=10:0:0
+#SBATCH --time=2:0:0
 module purge
 source ~/py39/bin/activate
 module load scipy-stack gcc cuda opencv
 MODEL_PATH=/home/hluo/scratch/models/model_job20/model_job20_epoch_99.pth
 DATA_DIR=/home/hluo/scratch/select_data_512
-RESULTS_DIR=/home/hluo/scratch/results/test1
+RESULTS_DIR=/home/hluo/scratch/results/test1a
 python test.py --model_path $MODEL_PATH --data_dir $DATA_DIR --results_dir $RESULTS_DIR
