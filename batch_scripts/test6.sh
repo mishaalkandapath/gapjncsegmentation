d8acf2ab6c4e4666954e22be0484f763afc2ed1e
Huayin Luo <hclover2003@gmail.com>
2024-05-22 16:32:36 -0400

    add tests

---
+++ batch_scripts/test6.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --job-name=test6
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.out
+#SBATCH --gpus-per-node=v100l:1
+#SBATCH --mem=64G
+#SBATCH --time=2:0:0
+module purge
+source ~/py39/bin/activate
+module load scipy-stack gcc cuda opencv
+MODEL_PATH=/home/hluo/scratch/models/model_job20e/model_job20e_epoch_99.pth
+DATA_DIR=/home/hluo/scratch/dauer_data_512
+RESULTS_DIR=/home/hluo/scratch/results/test6
+python ~/gapjncsegmentation/test.py --model_path $MODEL_PATH --data_dir $DATA_DIR --results_dir $RESULTS_DIR
