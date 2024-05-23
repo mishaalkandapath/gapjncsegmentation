3cb33eade3b3a3d693d1313205c81c8bfdab6f38
Huayin Luo <hclover2003@gmail.com>
2024-05-22 22:41:32 -0400

    add job

---
+++ batch_scripts/job51b.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --job-name=job51b
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.out
+#SBATCH --gpus-per-node=v100l:1
+#SBATCH --mem=64G
+#SBATCH --time=22:0:0
+module purge
+source ~/py39/bin/activate
+module load scipy-stack gcc cuda opencv
+EPOCHS=50
+BATCH_SIZE=1
+NUM_WORKERS=4
+LR=0.001
+DATA_DIR=/home/hluo/scratch/dauer_data_512
+MODEL_DIR=/home/hluo/scratch/models
+MODEL_NAME="model_job51b"
+LOAD_MODEL_PATH=/home/hluo/scratch/models/model_job51/model_job51_epoch_12.pth
+AUGMENT=True
+ALPHA=0.998
+GAMMA=3
+NUM_PREDICTIONS_TO_LOG=10
+python /home/hluo/gapjncsegmentation/unet_3d.py --epochs $EPOCHS --batch_size $BATCH_SIZE --lr $LR --data_dir $DATA_DIR --model_name $MODEL_NAME --num_workers $NUM_WORKERS --model_dir $MODEL_DIR --alpha $ALPHA --gamma $GAMMA --num_predictions_to_log $NUM_PREDICTIONS_TO_LOG --augment $AUGMENT
