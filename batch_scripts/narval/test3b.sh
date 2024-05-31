9d0e17cd9cef44210e96551ed8064f35e2d536c0
Huayin Luo <hclover2003@gmail.com>
2024-05-30 22:56:24 -0400

    add

---
+++ batch_scripts/narval/test3b.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --job-name=test3b
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.out
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --mem=64G
+#SBATCH --time=10:0:0
+module purge
+source ~/py39/bin/activate
+module load scipy-stack gcc cuda opencv
+MODEL_NAME=model_job92
+EPOCH=49
+MODEL_PATH=/home/hluo/scratch/models/${MODEL_NAME}/${MODEL_NAME}_epoch_${EPOCH}.pth
+DATA_DIR=/home/hluo/scratch/select_dauer_data_256
+RESULTS_DIR=/home/hluo/scratch/results/test3b
+FOLDER_TYPE=train
+python ~/gapjncsegmentation/test.py --model_path $MODEL_PATH --data_dir $DATA_DIR --results_dir $RESULTS_DIR
---
+++ batch_scripts/narval/test4.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --job-name=test4
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.out
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --mem=64G
+#SBATCH --time=10:0:0
+module purge
+source ~/py39/bin/activate
+module load scipy-stack gcc cuda opencv
+MODEL_NAME=model_job93
+EPOCH=49
+MODEL_PATH=/home/hluo/scratch/models/${MODEL_NAME}/${MODEL_NAME}_epoch_${EPOCH}.pth
+DATA_DIR=/home/hluo/scratch/select_dauer_data_512
+RESULTS_DIR=/home/hluo/scratch/results/test4
+FOLDER_TYPE=train
+python ~/gapjncsegmentation/test.py --model_path $MODEL_PATH --data_dir $DATA_DIR --results_dir $RESULTS_DIR --folder_type $FOLDER_TYPE
