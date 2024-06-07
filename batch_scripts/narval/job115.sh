16d85c90967e18ffb54f02cafc149593e8fdc047
Huayin Luo <hclover2003@gmail.com>
2024-06-07 14:52:50 -0400

    add job

---
+++ batch_scripts/narval/job115.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --job-name=job115
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.out
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --mem=64G
+#SBATCH --time=12:0:0
+module purge
+source ~/py39/bin/activate
+module load scipy-stack gcc cuda opencv
+LOSS_TYPE=focalt
+ALPHA=0.1
+BETA=0.9
+GAMMA=2
+EPOCHS=100
+BATCH_SIZE=2
+NUM_WORKERS=4
+LR=0.00002
+NUM_PREDICTIONS_TO_LOG=10
+AUGMENT=true
+LOAD_MODEL_PATH=/home/hluo/scratch/models/model_job107/model_job107_epoch_86.pth
+DATA_DIR=/home/hluo/scratch/select_dauer_data_512
+MODEL_DIR=/home/hluo/scratch/models
+RESULTS_DIR=/home/hluo/scratch/model_results
+MODEL_NAME="model_job115"
+python ~/gapjncsegmentation/unet_comboloss_no_wandb.py --epochs $EPOCHS --batch_size $BATCH_SIZE --lr $LR --data_dir $DATA_DIR --model_name $MODEL_NAME --num_workers $NUM_WORKERS --model_dir $MODEL_DIR --num_predictions_to_log $NUM_PREDICTIONS_TO_LOG --results_dir $RESULTS_DIR --augment $AUGMENT --load_model_path $LOAD_MODEL_PATH --alpha $ALPHA --beta $BETA --gamma $GAMMA --loss_type $LOSS_TYPE
