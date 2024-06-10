889aba9a682d6b856119b1adaed73a9e9ecf6aa5
Huayin Luo <hclover2003@gmail.com>
2024-06-10 11:42:16 -0400

    add job

--- getpreds.py
+++ getpreds.py
@@ -110,8 +110,8 @@ def main():
                     print("Saving visualizations", args.save_vis)
                 fig, ax = plt.subplots(4, depth, figsize=(15, 8), num=1)
                 for j in range(depth):
-                    ax[0, j].imshow(inputs[0, 0, j], cmap="gray")
-                    ax[1, j].imshow(labels[0, 0, j], cmap="gray")
+                    ax[0, j].imshow(inputs[0, 0, j].detach().cpu(), cmap="gray")
+                    ax[1, j].imshow(labels[0, 0, j].detach().cpu(), cmap="gray")
                     ax[2, j].imshow(binary_pred[j], cmap="gray")
                     im = ax[3, j].imshow(pred[j], cmap="viridis")
                 cbar = fig.colorbar(im, ax=ax[3, :])
