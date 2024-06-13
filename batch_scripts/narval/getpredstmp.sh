c9555e867ce7eca1944fad43771cd6b6afd06367
Huayin Luo <hclover2003@gmail.com>
2024-06-13 11:09:41 -0400

    split full img

--- batch_scripts/narval/getpredstmp.sh
+++ batch_scripts/narval/getpredstmp.sh
@@ -15,28 +15,31 @@ X_DIR=/home/hluo/scratch/filtered_0_50_3x512x512/original/train
 Y_DIR=/home/hluo/scratch/filtered_0_50_3x512x512/ground_truth/train
 
 
+
+
+MODEL_NAME=model_job124
+EPOCH=99
+MODEL_PATH=/home/hluo/scratch/models/${MODEL_NAME}/${MODEL_NAME}_epoch_${EPOCH}.pth
+
+
 module purge
 source ~/py39/bin/activate
 module load scipy-stack gcc cuda opencv
 X_DIR=/home/hluo/scratch/filtered_0_50_3x512x512/original
 Y_DIR=/home/hluo/scratch/filtered_0_50_3x512x512/ground_truth
+SAVECOMB=false
 BATCH_SIZE=1
 NUM_WORKERS=1
 SAVE_VIS=false
-SAVE2D=false
+SAVE2D=true
 SAVE3D=false
 SAVECOMB=false
 SUBVOL_DEPTH=3
 SUBVOL_HEIGHT=512
 SUBVOL_WIDTH=512
 USEALLSUBFOLDERS=true
-MODEL_NAME=model_job128c
-EPOCH=99
-MODEL_PATH=/home/hluo/scratch/models/${MODEL_NAME}/${MODEL_NAME}_epoch_${EPOCH}.pth
-SAVE_DIR=/home/hluo/scratch/tmp${MODEL_NAME}
+SAVE_DIR=/home/hluo/scratch/0_50_${MODEL_NAME}
 python /home/hluo/gapjncsegmentation/getpreds.py --useallsubfolders $USEALLSUBFOLDERS --x_dir $X_DIR --y_dir $Y_DIR --save_dir $SAVE_DIR --model_path $MODEL_PATH --num_workers $NUM_WORKERS --batch_size $BATCH_SIZE --save_vis $SAVE_VIS --save2d $SAVE2D --subvol_depth $SUBVOL_DEPTH --subvol_height $SUBVOL_HEIGHT --subvol_width $SUBVOL_WIDTH --savecomb $SAVECOMB
-
-
 
 
 module purge
@@ -54,9 +57,6 @@ SUBVOL_DEPTH=3
 SUBVOL_HEIGHT=512
 SUBVOL_WIDTH=512
 USEALLSUBFOLDERS=false
-MODEL_NAME=model_job120
-EPOCH=99
-MODEL_PATH=/home/hluo/scratch/models/${MODEL_NAME}/${MODEL_NAME}_epoch_${EPOCH}.pth
 SAVE_DIR=/home/hluo/scratch/100_110_${MODEL_NAME}
 PRED_MEMB=true
 python /home/hluo/gapjncsegmentation/getpreds.py --pred_memb $PRED_MEMB --useallsubfolders $USEALLSUBFOLDERS --x_dir $X_DIR --y_dir $Y_DIR --save_dir $SAVE_DIR --model_path $MODEL_PATH --num_workers $NUM_WORKERS --batch_size $BATCH_SIZE --save_vis $SAVE_VIS --save2d $SAVE2D --subvol_depth $SUBVOL_DEPTH --subvol_height $SUBVOL_HEIGHT --subvol_width $SUBVOL_WIDTH --savecomb $SAVECOMB
