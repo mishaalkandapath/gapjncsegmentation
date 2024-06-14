52027bf8a35955d1e366d4796e0faf568d5b8d62
Huayin Luo <hclover2003@gmail.com>
2024-06-14 10:46:13 -0400

    add all

--- batch_scripts/narval/job142.sh
+++ batch_scripts/narval/job142.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
-#SBATCH --job-name=job141c
+#SBATCH --job-name=job142
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.out
 #SBATCH --gpus-per-node=a100:1
@@ -28,5 +28,5 @@ DATA_DIR=/home/hluo/scratch/filtered_0_50_3x512x512
 CELLMASK_DIR=/home/hluo/scratch/filtered_0_50_3x512x512/cell_mask
 MODEL_DIR=/home/hluo/scratch/models
 RESULTS_DIR=/home/hluo/scratch/model_results
-MODEL_NAME="model_job141c"
+MODEL_NAME="model_job142"
 python ~/gapjncsegmentation/unet_comboloss_no_wandb.py --cellmask_dir $CELLMASK_DIR --epochs $EPOCHS --batch_size $BATCH_SIZE --lr $LR --data_dir $DATA_DIR --model_name $MODEL_NAME --num_workers $NUM_WORKERS --model_dir $MODEL_DIR --num_predictions_to_log $NUM_PREDICTIONS_TO_LOG --results_dir $RESULTS_DIR --augment $AUGMENT --load_model_path $LOAD_MODEL_PATH --alpha $ALPHA --beta $BETA --gamma $GAMMA --loss_type $LOSS_TYPE --seed $SEED
