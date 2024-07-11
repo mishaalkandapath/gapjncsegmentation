35558420dc705f82488f05db7e3bd04000bbc51c
Huayin Luo <hclover2003@gmail.com>
2024-07-11 10:08:50 -0400

    add

--- batch_scripts/julynarval/stitch1.sh
+++ batch_scripts/julynarval/stitch1.sh
@@ -18,10 +18,13 @@ SAVE_DIR="/home/hluo/scratch/stitchedpreds/${SLICES}_${MODELNAME}_epoch_${EPOCH}
 USE_LINES=false
 SHOW_IMG=false
 STITCH2d=true
-START_S=0
-END_S=30
+START_S=150
+END_S=180
 START_X=0
 START_Y=0
 END_X=9216
 END_Y=8192
-python ~/gapjncsegmentation/stitch.py --stitch2d $STITCH2d --pred_dir $PRED_DIR --save_dir $SAVE_DIR --show_img $SHOW_IMG --use_lines $USE_LINES --start_s $START_S --end_s $END_S --start_x $START_X --end_x $END_X --start_y $START_Y --end_y $END_Y
+FILENAME_REGEX_PREFIX="SEM_dauer_2_export_s"
+FILENAME_REGEX_MIDDLE=".png_"
+FILENAME_REGEX_SUFFIX=".png "
+python ~/gapjncsegmentation/stitch.py --filename_regex_prefix $FILENAME_REGEX_PREFIX --filename_regex_middle $FILENAME_REGEX_MIDDLE --filename_regex_suffix $FILENAME_REGEX_SUFFIX --stitch2d $STITCH2d --pred_dir $PRED_DIR --save_dir $SAVE_DIR --show_img $SHOW_IMG --use_lines $USE_LINES --start_s $START_S --end_s $END_S --start_x $START_X --end_x $END_X --start_y $START_Y --end_y $END_Y
