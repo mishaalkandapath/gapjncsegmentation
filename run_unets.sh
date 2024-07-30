453a2a342206049400793baeb8f9bfe252e1c227
mishaalkandapath <mishaalhere@gmail.com>
2024-07-30 10:44:41 -0400

    some more command templates

--- run_unets.sh
+++ run_unets.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=%x-%j.out
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --mem=128G
-#SBATCH --time=6:30:0
+#SBATCH --time=10:0:0
 #SBATCH --signal=SIGUSR1@90
 #SBATCH --account=def-mzhen
 module purge
@@ -23,8 +23,21 @@ wandb offline
 #python /home/mishaalk/projects/def-mzhen/mishaalk/gapjncsegmentation/unet.py --split --dataset tiny --batch_size 100
 
 # python unet.py --dataset new3d --td --gendice --batch_size 10
-python unet.py --dataset new3d --td --batch_size 10 --dicefocal --focalweight 0.2 #-- 14 hours!
+# python unet.py --dataset new --batch_size 16 --dicefocal --focalweight 0 #-- 14 hours!- 29926099
 #python unet.py --dataset tiny --dice --batch_size 100 --mask_neurons -- 4 hrs
 #python unet.py --dataset new --mask_neurons --gendice 10 hrs?
 # python unet.py --dataset tiny --batch_size 100 --mask_neurons --mask_mito --epochs 500 EPOCHS REMEMVER TO CHANGE - about 10 hrs
+# python unet.py --dataset new --batch_size 10 --tversky --mask_feat 
 #
+# python unet.py --dataset new --batch_size 16 --gendice --mem_feat --aug
+# python unet.py --dataset new --batch_size 16 --gendice --resnet --mem_feat --aug
+# python unet.py --dataset new --batch_size 16 --gendice --mem_feat
+# python unet.py --dataset new --batch_size 16 --focal --resnet --mask_neurons #--mem_feat --aug
+# python unet.py --special
+# python unet.py --pred_mem --gendice --dataset new
+
+# python unet.py --dataset new --batch_size 16 --gendice --mem_feat
+
+#some finetuning
+#python unet.py --dataset new --batch_size 16 --gendice --model_name 2d_gd_mem_run1/model5_epoch71.pk1 --finetune_dirs /home/mishaalk/scratch/gapjunc/train_datasets/finetune_split --aug
+python unet.py --dataset new --batch_size 16 --focal --fn_rwt --model_name 2d_gd_mem_run1/model5_epoch71.pk1
